b6a1dba30b5c4930f49a546e66ce6caa12e49125
warano02 <carineteoi@gmail.com>
2026-04-13 07:38:08 +0100

    lecon10:create post request

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-findAll(){
-return "this action return all the coffees"
-}
+  @Get()
+  findAll() {
+    return 'this action return all the coffees';
+  }
 
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action return ${id} coffee`;
+  }
 
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
 }
