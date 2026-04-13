ca9a8d3e863dad99d049f21e2b1b29e8257c896b
warano02 <carineteoi@gmail.com>
2026-04-13 09:29:52 +0100

    lecon16: generate data transfert object(dto) to help for data validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
-@Module({
+@Module({ 
   imports: [CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
