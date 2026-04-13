939160433372a6bf46cbeec38b46b5b66d322242
warano02 <carineteoi@gmail.com>
2026-04-13 21:21:14 +0100

    lecon 23: add docker compose for postgre db, typeOrm for db management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({ 
-  imports: [CoffeesModule],
+@Module({
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: 'password',
+      database: 'coffety',
+      autoLoadEntities: true,
+      synchronize: true,
+      port: 5432,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
