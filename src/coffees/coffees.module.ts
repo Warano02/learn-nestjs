d4bdb161d3d533c83ebbe3b66074cefb9fd6b79f
warano02 <carineteoi@gmail.com>
2026-04-13 22:00:27 +0100

    lecon 26: add flavor entity and intenciate a realtion between it and coffees entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,11 +3,11 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entity/coffee.entity';
+import { Flavour } from './entity/flavours.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavour])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
-
 export class CoffeesModule {}
--- src/coffees/entity/coffee.entity.ts
+++ src/coffees/entity/coffee.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavour } from './flavours.entity';
 
 @Entity()
 export class Coffee {
@@ -6,10 +13,11 @@ export class Coffee {
   id: number;
   @Column()
   name: string;
-  
+
   @Column()
   brand: string;
 
-  @Column('json', { nullable: true })
+  @JoinTable()
+  @ManyToMany((type) => Flavour, (flavour) => flavour.coffees)
   flavors: string[];
 }
