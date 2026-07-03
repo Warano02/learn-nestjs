6f739f0443d0395af00c497879a6211b22806835
warano02 <carineteoi@gmail.com>
2026-07-03 12:41:32 +0100

    lecon44: add config file and extract it using @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,21 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      username: 'postgres',
-      password: 'password',
-      database: 'coffety',
+      host: process.env.DATABASE_HOST,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
-      port: 5432,
+      port: +(process.env.DATABASE_PORT || 5432),
     }),
     CoffeeRatingModule,
     DatabaseModule,
