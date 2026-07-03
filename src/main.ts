9f4772205b70157d7aab8b038a9218e92b468723
warano02 <carineteoi@gmail.com>
2026-07-03 14:01:10 +0100

    lecon55: creating a public system for guard control using metadata by creating our custom decorator

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalGuards(new ApiKeyGuard())
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT ?? 3000);
 }
